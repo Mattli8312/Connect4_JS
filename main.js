6f419429ad4b890bb614a88783563ab25010cb51
Matthew Lian <mattli8312@gmail.com>
2022-01-06 17:21:07 -0600

    updated framework code

--- main.js
+++ main.js
@@ -1,35 +1,67 @@
 const board = document.getElementById('gameboard');
-const width = 10;
-const height = 7;
+const width = 7;
+const height = 6;
 const GB = new GameBoard(width, height);
+let red_turn = true;
+let winner = null;
+
 GB.Initialize();
 
-const New_Div = (attributes = []) => {
-    const result = document.createElement("div");
-    for(const a of attributes){
-        result.setAttribute(a[0], a[1]);
+const Winner_State = () => {
+    console.log(winner)
+    const list = document.getElementsByClassName('Cell');
+    for(const li of list){
+        const new_element = li.cloneNode(true);
+        li.parentNode.replaceChild(new_element, li);
     }
-    return result;
 }
 
-const Get_Element = (id = "") => {
-    let result = document.getElementById(id);
-    if(result !== undefined) return result;
-    else return false;
-}
+const InitializeGameBoard = () => {
 
-const RenderGameBoard = () => {
     for(let i = 0; i < GB.GetHeight(); i++){
+
         let new_row = New_Div([["id", "row"]]);
+
         for(let j = 0; j < GB.GetWidth(); j++){
-            let new_cell = New_Div([["id", i + ',' + j]]);
-            new_cell.style.width = new_cell.style.height = "2vw";
-            new_cell.style.background = "grey";
-            new_cell.style.border = "10px solid black";
+
+            let new_cell = New_Div([["id", i + ',' + j], ["class", "Cell"]]);
+
+            new_cell.addEventListener('mousedown', ()=>{
+
+                switch(HandleMove(GB, red_turn, j)){
+
+                    case 'invalid': return;
+                    case 'winner': winner = red_turn ? 'red' : 'yellow'; break;
+                    case 'continue': break;
+                    default: break;
+                }
+            
+                red_turn ^= true;
+            
+                RenderGameBoard();
+
+                if(winner !== null) Winner_State();
+            })
             new_row.appendChild(new_cell);
         }
         board.appendChild(new_row);
     }
 }
 
-RenderGameBoard()
+const RenderGameBoard = () => {
+    for(let i = 0; i < GB.GetHeight(); i++){
+        for(let j = 0; j < GB.GetWidth(); j++){
+            switch(GB.AccessBoardElement(i,j)){
+                case 'r':
+                    Get_Element(i+','+j).setAttribute('filled', 'red'); break;
+                case 'y':
+                    Get_Element(i + ',' + j).setAttribute('filled', 'yellow'); break;
+                default:
+                    Get_Element(i+','+j).setAttribute('filled', 'grey'); 
+            }
+        }
+    }
+}
+
+InitializeGameBoard();
+RenderGameBoard();
