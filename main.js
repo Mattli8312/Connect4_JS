da3f88c1a19b649ad31f373dc3550e6fcf81845d
Matthew Lian <mattli8312@gmail.com>
2022-01-09 16:26:45 -0600

    Updated Main.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const { faListAlt } = require("@fortawesome/free-solid-svg-icons");
+
 const board = document.getElementById('gameboard');
 const width = 7;
 const height = 6;
@@ -50,23 +52,32 @@ const InitializeGameBoard = async() => {
 
             let new_cell = New_Div([["id", i + ',' + j], ["class", "Cell"]]);
 
-            new_cell.addEventListener('mousedown', async()=>{
-
-                let value = await Execute_Move(j);
-                
-                if(value) return;
-
-                let CPU_turn = Execute_MiniMax(GB, red_turn);
-
-                value = await Execute_Move(CPU_turn);
-
-                if(value) return;
-
-            })
             new_row.appendChild(new_cell);
         }
         board.appendChild(new_row);
     }
+}
+const Initialize_CPU_Mode = () => {
+
+    let list_ = document.getElementsByClassName('Cell');
+    
+    for(const li of list_) {
+
+        li.addEventListener('mousedown', async()=>{
+
+            let value = await Execute_Move(j);
+            
+            if(value) return;
+
+            let CPU_turn = Execute_MiniMax(GB, red_turn);
+
+            value = await Execute_Move(CPU_turn);
+
+            if(value) return;
+
+        })
+    }
+
 }
 
 const RenderGameBoard = () => {
@@ -85,6 +96,7 @@ const RenderGameBoard = () => {
 }
 
 InitializeGameBoard();
+Initialize_CPU_Mode();
 /**
  * Updated with animations
  */
